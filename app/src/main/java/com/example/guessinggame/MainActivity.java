95086532f3235d000396c45de85aa30cf1b1d79f
thiegorechi <rechithiego@gmail.com>
2025-04-04 13:40:50 -0600

    Add randomization, scoring logic, and color feedback
    Implement random questions, first-try scoring, and color feedback on answer buttons

--- app/src/main/java/com/example/guessinggame/MainActivity.java
+++ app/src/main/java/com/example/guessinggame/MainActivity.java
@@ -17,17 +17,16 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
-    // create array to store questions
-    Questions[] questions = new Questions[20]; // array of Json data (must be assigned with value from settings)
+    Questions[] questions = new Questions[20];
     int questionIndex = 0;
     Button op1, op2, op3, op4, settings;
-
     TextView questionTitle, scoreCounter;
-
-    ImageView imgFlag; // assignment 2
+    ImageView imgFlag;
     int score = 0;
 
     @Override
@@ -36,53 +35,47 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        //make references of components
         questionTitle = findViewById(R.id.question_Title);
         op1 = findViewById(R.id.btn_option1);
         op2 = findViewById(R.id.btn_option2);
         op3 = findViewById(R.id.btn_option3);
         op4 = findViewById(R.id.btn_option4);
-        settings = findViewById(R.id.btn_Settings); // assignment 2
-        imgFlag  = findViewById(R.id.img_flag); // assignment 2
-        scoreCounter    = findViewById(R.id.tv_score2);
+        settings = findViewById(R.id.btn_Settings);
+        imgFlag = findViewById(R.id.img_flag);
+        scoreCounter = findViewById(R.id.tv_score2);
 
-        // assignment 2
         loadQuestionsFromJSON();
-        // assignment 2
+        Collections.shuffle(Arrays.asList(questions)); // 🔀 Randomiza as perguntas
         loadQuestion();
-
     }
 
-    // this method is embedded from Android by default and now is overriding
-    protected void onStart(){ // on loading the screen for the first time
+    @Override
+    protected void onStart() {
         super.onStart();
-        loadQuestion(); // method to load questions of game
+        loadQuestion();
         updateScore();
     }
 
-   @Override
+    @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        score = savedInstanceState.getInt("SCORE"); // restore saved score (from onSaveInstanceState())
-        questionIndex = savedInstanceState.getInt("QUESTION"); // restore saved question (from onSaveInstanceState())
-
-        //PARCELABLE - This line retrieves an array of objects that were previously saved as Parcelable in onSaveInstanceState().
+        score = savedInstanceState.getInt("SCORE");
+        questionIndex = savedInstanceState.getInt("QUESTION");
         Parcelable[] parcelableArray = savedInstanceState.getParcelableArray("QUESTIONS");
 
-        // read this array and fill the array Questions[] destroyed when the screen was rotated
-        if (parcelableArray != null){
-            for (int i = 0; i < parcelableArray.length; i++){
+        if (parcelableArray != null) {
+            for (int i = 0; i < parcelableArray.length; i++) {
                 questions[i] = (Questions) parcelableArray[i];
             }
         }
-        loadQuestion(); // method to load questions of game
+        loadQuestion();
         updateScore();
     }
 
-    // This method goes to the next Question and accessed by onclick button event
-    public void nextQuestion(View view) { // questionIndex == 20 (must be assigned with value from settings)
-        if (questionIndex == 20) Toast.makeText(this, "It's The final question", Toast.LENGTH_SHORT).show();
-        else {
+    public void nextQuestion(View view) {
+        if (questionIndex == 19) {
+            Toast.makeText(this, "It's the final question", Toast.LENGTH_SHORT).show();
+        } else {
             questionIndex++;
             loadQuestion();
             if (questions[questionIndex].answered) {
@@ -91,20 +84,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // This method goes back to the previous Question and accessed by onclick button event
-    public void previousQuestion(View view){ // method used onclick button event "previous Button"
-        if (questionIndex == 0) Toast.makeText(this, "You're at the first question", Toast.LENGTH_SHORT).show(); // when user tries to click on button "Previous" and he is on the first question
-        else {
+    public void previousQuestion(View view) {
+        if (questionIndex == 0) {
+            Toast.makeText(this, "You're at the first question", Toast.LENGTH_SHORT).show();
+        } else {
             questionIndex--;
             loadQuestion();
-            // Verify if the question has already answered
             if (questions[questionIndex].answered) {
                 Toast.makeText(this, "Question already answered!", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-    //assignment 2 - This method loads all questions only once and just keeps information of flags on memory
     private void loadQuestionsFromJSON() {
         try {
             InputStream is = getAssets().open("questions.json");
@@ -138,65 +129,65 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // This method loads questions and stores on buttons
     private void loadQuestion() {
-        Questions current = questions[questionIndex]; // Assignment 2 - Get the current question from the questions array using the current index
+        Questions current = questions[questionIndex];
         questionTitle.setText(current.questionAsk);
-        imgFlag.setImageResource(current.imageResId); // Assignment 2 - Set the flag image according to the current question
-        op1.setText(questions[questionIndex].option1);
-        op2.setText(questions[questionIndex].option2);
-        op3.setText(questions[questionIndex].option3);
-        op4.setText(questions[questionIndex].option4);
+        imgFlag.setImageResource(current.imageResId);
+        op1.setText(current.option1);
+        op2.setText(current.option2);
+        op3.setText(current.option3);
+        op4.setText(current.option4);
+
+        // Resetar a cor dos botões
+        op1.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
+        op2.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
+        op3.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
+        op4.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
     }
 
-
-    private void updateScore(){
+    private void updateScore() {
         scoreCounter.setText(Integer.toString(score));
     }
-    public void pickAnswer(View view) { // // method used onclick button event - linked to answer's buttons
+
+    public void pickAnswer(View view) {
         Button button = (Button) view;
 
-        // Verify if the question has already answered and impedes the user answer again and then update score wrongly
         if (questions[questionIndex].answered) {
             Toast.makeText(this, "Question already answered !", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        if (button.getText().toString().equals(questions[questionIndex].correctAnswer)) { // correct Answer ? To be used with Json strings
-            if (!questions[questionIndex].answered) { // if questions is answered
-                score++;
-            }
-            questions[questionIndex].answered = true;
-            Toast.makeText(this, "Correct !", Toast.LENGTH_SHORT).show();
+        String selectedAnswer = button.getText().toString();
+        String correctAnswer = questions[questionIndex].correctAnswer;
 
-            loadQuestion();
-            updateScore();
+        if (selectedAnswer.equals(correctAnswer)) {
+            score++;
+            button.setBackgroundColor(getResources().getColor(android.R.color.holo_green_light));
+            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
+        } else {
+            button.setBackgroundColor(getResources().getColor(android.R.color.holo_red_light));
+            Toast.makeText(this, "Incorrect, try again!", Toast.LENGTH_SHORT).show();
         }
-        else {
-            Toast.makeText(this, "Incorrect, try again !", Toast.LENGTH_SHORT).show();
-        }
+
+        questions[questionIndex].answered = true;
+        updateScore();
     }
 
-    // This method resets the Game and is called by 2 ways (activity_main and activity_settings)
     public void resetGame(View view) {
         score = 0;
         questionIndex = 0;
 
-        // Reset the state of questions
         for (Questions question : questions) {
             question.answered = false;
         }
+
+        Collections.shuffle(Arrays.asList(questions)); // embaralhar novamente
         loadQuestion();
         updateScore();
     }
 
-    //assignment 2 - This method calls the activity_settings
     public void openSettingsActivity(View view) {
         Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
         startActivity(intent);
     }
-
-
-} // end of MainActivity.java
-
-
+}
